454e73a66b4520e92dbd1cdbdd353db63d2d7469
barhoomi <ialkhayatt@outlook.com>
2025-11-18 18:07:32 +0300

    Solve Exercise 2.4

--- part2/section-a/src/App.jsx
+++ part2/section-a/src/App.jsx
@@ -1,6 +1,6 @@
 const Header = ({course}) => {
   return (
-    <h1>{course}</h1>
+    <h2>{course}</h2>
   )
 }
 
@@ -43,29 +43,61 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return(
+    <div>
+        <h1>Web Development Curriculum</h1>
+        {courses.map((c)=>
+            <div key={c.id}>
+                <Course course={c}/>
+            </div>
+        )}
+    </div>
+  )
 }
 
 
