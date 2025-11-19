caca4e7fb34e933d931a428deba190614a1f80a6
barhoomi <ialkhayatt@outlook.com>
2025-11-19 16:05:09 +0300

    Solve exercise 2.9 - phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,10 +10,14 @@ const List = ({ elements }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -26,9 +30,19 @@ const App = () => {
     }
   }
 
+  const filterPeople=(f) => {
+    return persons.filter(p => p.name.toLowerCase().includes(f))
+  }
+
+  const shownPeople = filterPeople(newFilter.toLowerCase())
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={(event)=>{event.preventDefault()}}>
+        filter shown with: <input onChange={event=>setFilter(event.target.value)}/>
+      </form>
+
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={(event) => setNewName(event.target.value)} />
@@ -44,7 +58,7 @@ const App = () => {
 
 
       <h2>Numbers</h2>
-      <List elements={persons} />
+      <List elements={shownPeople} />
     </div>
 
   )
