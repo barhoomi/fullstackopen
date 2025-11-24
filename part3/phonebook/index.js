351fb383aa1efff9e3e8803b2b674b72e21d3964
barhoomi <ialkhayatt@outlook.com>
2025-11-24 16:22:12 +0300

    Solve exercise 3.5 - phonebook backend part 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,61 +1,86 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+let persons = [{
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-app.get("/api/persons",(request,response)=>{
+app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
+app.post("/api/persons", (request, response) => {
+    const body = request.body
 
-app.get("/api/persons/:id",(request,response)=>{
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const newPerson = {
+        "number": body.number,
+        "name": body.name,
+        "id": String(parseInt(Math.random() * 100000))
+    }
+
+    persons = persons.concat(newPerson)
+    
+    console.log(`successfully added ${newPerson}`)
+    response.json(newPerson)
+
+})
+
+
+app.get("/api/persons/:id", (request, response) => {
     const id = request.params.id
     const person = persons.find(p => p.id == id)
-    if(person == null){
+    if (person == null) {
         console.log(`person with ID:${id} not found`)
         response.status(404).end()
-    }
-    else{
+    } else {
         response.json(person)
     }
 })
 
 
-app.delete("/api/persons/:id",(request,response)=>{
+app.delete("/api/persons/:id", (request, response) => {
     const id = request.params.id
     const person = persons.find(p => p.id == id)
-    if(person == null){
+    if (person == null) {
         console.log(`person with ID:${id} does not exist, or was already deleted`)
         response.status(404).end()
-    }
-    else{
+    } else {
         persons = persons.filter(p => p.id !== id)
         response.status(204).end()
     }
 })
 
-app.get("/info",(request,response)=>{
+app.get("/info", (request, response) => {
     response.send(
         `<p>Phonebook has info for ${persons.length} people</p>
         ${Date()}`
