07825357b6cdc1cda44912aa3c1a73875750c43f
barhoomi <ialkhayatt@outlook.com>
2025-11-25 11:41:41 +0300

    add static express middleware

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("dist"))
 
 
 
@@ -27,9 +28,6 @@ let notes = [{
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get("/api/notes/:id", (request, response) => {
   const id = request.params.id
