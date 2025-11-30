aa764417b3c82437975d35f3783abb708c839836
barhoomi <ialkhayatt@outlook.com>
2025-11-30 12:35:15 +0300

    Solve 3.20*: Phonebook database, step 8

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -18,7 +18,17 @@ const personSchema = new mongoose.Schema({
         type: String,
         minLength: 3
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v){
+                    return /^\d{2,3}-\d{1,}$/.test(v);
+                },
+                message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    }
 })
 
 const Person = mongoose.model("Person", personSchema)
