330b9130a73e737d55522402cac1ffc063fad1a6
barhoomi <ialkhayatt@outlook.com>
2025-11-30 13:48:01 +0300

    refactor part 3 - notes

--- part3/notes/models/note.js
+++ part3/notes/models/note.js
@@ -1,16 +1,5 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url, {
-        family: 4
-    })
-    .then(result => {
-        console.log('connected to MongoDB')
-    }).catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+const mongoose = require("mongoose")
+mongoose.set("strictQuery", false)
 
 const noteSchema = new mongoose.Schema({
     content: {
@@ -21,7 +10,7 @@ const noteSchema = new mongoose.Schema({
     important: Boolean,
 })
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
@@ -29,6 +18,5 @@ noteSchema.set('toJSON', {
     }
 })
 
-const Note = mongoose.model('Note', noteSchema)
-
+const Note = mongoose.model("Note", noteSchema)
 module.exports = Note
