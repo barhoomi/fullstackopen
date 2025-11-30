da70bd85f8875db150b9f95cd4e820f5d84c06f5
barhoomi <ialkhayatt@outlook.com>
2025-11-30 16:47:48 +0300

    Solve 4.2 Blog List, step 2

---
+++ part4/blog_list/app.js
@@ -0,0 +1,35 @@
+// Import required modules
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+
+// Create an Express application
+const app = express()
+
+// Connect to MongoDB
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+
+// Middleware setup
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
