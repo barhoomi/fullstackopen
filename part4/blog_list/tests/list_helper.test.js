bfa9d5f558fd5595b239482d587310c6300105dc
barhoomi <ialkhayatt@outlook.com>
2025-12-01 13:22:59 +0300

    Solve 4.5*: Helper Functions and Unit Tests, step 3

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -1,6 +1,6 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const { dummy, totalLikes } = require('../utils/list_helper')
+const { dummy, totalLikes, favouriteBlog } = require('../utils/list_helper')
 
 describe('dummy function', () => {
     test('dummy returns one', () => {
@@ -54,5 +54,84 @@ describe('total likes', () => {
 
         const result = totalLikes(blogs)
         assert.strictEqual(result, 12)
+    })
+})
+
+
+describe("Favourite Blog",()=>{
+    test("Favourite blog when blogs list is empty", ()=>{
+        const blogs = []
+        assert.strictEqual(favouriteBlog(blogs),0)
+    })
+
+    test("Favourite blog when blogs list has one blog", ()=>{
+        const blogs = [
+            {
+                _id: "5a422aa71b54a676234d17f8",
+                title: "Go To Statement Considered Harmful",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.ucl.ac.uk/teaching/academic/academic-skills/academic-writing/academic-writing-structure",
+                likes: 5
+            }
+        ]
+        assert.deepStrictEqual(favouriteBlog(blogs),{
+                _id: "5a422aa71b54a676234d17f8",
+                title: "Go To Statement Considered Harmful",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.ucl.ac.uk/teaching/academic/academic-skills/academic-writing/academic-writing-structure",
+                likes: 5
+            })
+    })
+
+    test("Favourite blog when blogs list has two blogs", ()=>{
+        const blogs = [
+            {
+                _id: "5a422aa71b54a676234d17f8",
+                title: "Go To Statement Considered Harmful",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.ucl.ac.uk/teaching/academic/academic-skills/academic-writing/academic-writing-structure",
+                likes: 5
+            },
+            {
+                _id: "5a422a851b54a676234d17f7",
+                title: "React patterns",
+                author: "Michael Chan",
+                url: "https://reactpatterns.com/",
+                likes: 7
+            }
+        ]
+        assert.deepStrictEqual(favouriteBlog(blogs),{
+                _id: "5a422a851b54a676234d17f7",
+                title: "React patterns",
+                author: "Michael Chan",
+                url: "https://reactpatterns.com/",
+                likes: 7
+            })
+    })
+
+    test("Favourite blog when blogs list has two blogs with switched order", ()=>{
+        const blogs = [
+            {
+                _id: "5a422a851b54a676234d17f7",
+                title: "React patterns",
+                author: "Michael Chan",
+                url: "https://reactpatterns.com/",
+                likes: 7
+            },
+            {
+                _id: "5a422aa71b54a676234d17f8",
+                title: "Go To Statement Considered Harmful",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.ucl.ac.uk/teaching/academic/academic-skills/academic-writing/academic-writing-structure",
+                likes: 5
+            }
+        ]
+        assert.deepStrictEqual(favouriteBlog(blogs),{
+                _id: "5a422a851b54a676234d17f7",
+                title: "React patterns",
+                author: "Michael Chan",
+                url: "https://reactpatterns.com/",
+                likes: 7
+            })
     })
 })
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum,blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+    const sortedBlogs = blogs.sort((blogA, blogB)=> blogB.likes - blogA.likes)
+    //console.log("sorted blogs: ",sortedBlogs)
+    return blogs.length == 0? 0: sortedBlogs[0]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
