d73ef6d5e39e603fdd7f29769cb6f5237dc5e739
barhoomi <ialkhayatt@outlook.com>
2025-12-01 13:50:04 +0300

    Solve 4.6*: Helper Functions and Unit Tests, step 4

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -1,6 +1,6 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
-const { dummy, totalLikes, favouriteBlog } = require('../utils/list_helper')
+const { dummy, totalLikes, favouriteBlog, mostBlogs } = require('../utils/list_helper')
 
 describe('dummy function', () => {
     test('dummy returns one', () => {
@@ -133,5 +133,35 @@ describe("Favourite Blog",()=>{
                 url: "https://reactpatterns.com/",
                 likes: 7
             })
+    })
+})
+
+
+describe("Most blogs", ()=>{
+    test("Most blogs given small array", ()=>{
+        const blogs = [
+            {
+                _id: "5a422a851b54a676234d17f7",
+                title: "React patterns",
+                author: "Michael Chan",
+                url: "https://reactpatterns.com/",
+                likes: 7
+            },
+            {
+                _id: "5a422a851b54a676234d17f7",
+                title: "React patterns",
+                author: "Michael Chan",
+                url: "https://reactpatterns.com/",
+                likes: 7
+            },
+            {
+                _id: "5a422aa71b54a676234d17f8",
+                title: "Go To Statement Considered Harmful",
+                author: "Edsger W. Dijkstra",
+                url: "http://www.ucl.ac.uk/teaching/academic/academic-skills/academic-writing/academic-writing-structure",
+                likes: 5
+            }
+        ]
+        assert.deepStrictEqual(mostBlogs(blogs),{"author":"Michael Chan","blogs":2})
     })
 })
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -3,17 +3,36 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((sum,blog) => sum + blog.likes, 0)
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
 const favouriteBlog = (blogs) => {
-    const sortedBlogs = blogs.sort((blogA, blogB)=> blogB.likes - blogA.likes)
+    const sortedBlogs = blogs.sort((blogA, blogB) => blogB.likes - blogA.likes)
     //console.log("sorted blogs: ",sortedBlogs)
-    return blogs.length == 0? 0: sortedBlogs[0]
+    return blogs.length == 0 ? 0 : sortedBlogs[0]
+}
+
+const mostBlogs = (blogs) => {
+    var authors = blogs.reduce((sum, blog) => {
+        //console.log("author: ", blog.author, " sum: ", sum)
+        if (blog.author in sum) {
+            sum[blog.author] += 1
+        }
+        else {
+            sum[blog.author] = 1
+        }
+        return sum
+    }, {})
+    var authors = Object.keys(authors).map((key) => [key, authors[key]])
+
+    const sortedAuthors = authors.sort((authorA, authorB) => authorB[1] - authorA[1])
+    console.log("sorted authors: ",sortedAuthors)
+    return {"author":sortedAuthors[0][0], "blogs":sortedAuthors[0][1]}
 }
 
 module.exports = {
-  dummy,
-  totalLikes,
-  favouriteBlog
+    dummy,
+    totalLikes,
+    favouriteBlog,
+    mostBlogs
 }
