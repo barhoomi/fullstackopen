eecc3b5dc9ff8ff4af106bbe0df6a47bfec353b5
barhoomi <ialkhayatt@outlook.com>
2025-12-09 14:47:31 +0300

    Solve 4.11*: Blog List Tests, step 4

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -42,7 +42,7 @@ test("unique identifier property of the blog posts is named id", async () => {
     assert.strictEqual(0, blogs.body.filter(blog => blog["_id"] !== undefined).length)
 })
 
-test.only("making an HTTP POST request to the /api/blogs URL successfully creates a new blog post", async () => {
+test("making an HTTP POST request to the /api/blogs URL successfully creates a new blog post", async () => {
 
     const newPostId = "892c44bdfecbea804ddd6bea"
     const newPost = {
@@ -50,7 +50,7 @@ test.only("making an HTTP POST request to the /api/blogs URL successfully create
         author: "Derek Sivers",
         url: "https://sive.rs/ayw8",
         likes: 4,
-        _id:newPostId
+        _id: newPostId
     }
 
     await api
@@ -63,8 +63,27 @@ test.only("making an HTTP POST request to the /api/blogs URL successfully create
     const blogs = await api.get("/api/blogs")
     const newBlog = await api.get(`/api/blogs/${newPostId}`).expect(200)
     assert.strictEqual(blogs.body.length, helper.initialBlogs.length + 1)
-    assert.notStrictEqual(newBlog,undefined)
+    assert.notStrictEqual(newBlog, undefined)
     logger.info(blogs.body)
+})
+
+test.only("missing likes property defaults to 0", async () => {
+    const newPostId = "892c44bdfecbea804ddd6bea"
+    const newPost = {
+        title: "You make your perfect world",
+        author: "Derek Sivers",
+        url: "https://sive.rs/ayw8",
+        _id: newPostId
+    }
+
+    await api
+        .post("/api/blogs")
+        .send(newPost)
+        .expect(201)
+        .expect("Content-Type", /application\/json/)
+
+    const newBlog = await api.get(`/api/blogs/${newPostId}`).expect(200)
+    assert.strictEqual(newBlog.body.likes,0)
 })
 
 after(async () => {
